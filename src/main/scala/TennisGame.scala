d98a331ddd5ff3851002e8af46e99ad7b862adc1
arinconstrio <arincon@stratio.com>
2015-03-05 15:03:27 +0100

    refactor

--- src/main/scala/TennisGame.scala
+++ src/main/scala/TennisGame.scala
@@ -5,10 +5,37 @@ class TennisGame(player1: Player, player2: Player) {
   val desc = Array("love", "fifteen", "thirty", "forty")
 
   def score = {
-    if (player1.score <= 3 && player2.score <= 3)
-      desc(player1.score) + ", " + desc(player2.score)
-    else if (player1.score > player2.score) "advantage %s".format(player1.noun) else "advantage %s".format(player2.noun)
+    if (isDeuce) "deuce"
+    else {
+      def leader = getLeader
+      def looser = getLooser(leader _)
+
+      if (isLeaderWinner(leader _, looser _)) "%s won".format(leader.noun)
+      else if (player1.score <= 3 && player2.score <= 3) desc(player1.score) + ", " + desc(player2.score)
+      else advantagePrint(leader)
+    }
+
   }
 
+  private def getLooser(leader: () => Player): Player = {
+    if (leader() == player1) player2 else player1
+  }
 
+  private def getLeader: Player = {
+    if (player1.score > player2.score) player1 else player2
+  }
+
+  private def isLeaderWinner(leader: () => Player, looser: () => Player): Boolean = {
+    leader().score > 4 && leader().score - looser().score >= 2
+  }
+
+  private def isDeuce: Boolean = {
+    player1.score >= 3 && player1.score == player2.score
+  }
+
+  private def advantagePrint(player: Player): String = {
+    "advantage %s".format(player.noun)
+  }
 }
+
+
--- src/test/scala/TennisGameTest.scala
+++ src/test/scala/TennisGameTest.scala
@@ -51,4 +51,38 @@ class TennisGameTest extends UnitSpec{
     (1 to 4).foreach(x => sarah.winBall())
     game.score should be("advantage Sarah")
   }
+  "Deuce" should "be description when at least three points have been scored by each player and the scores are equal" in {
+    val victor = Player("Victor")
+    val sarah = Player("Sarah")
+    val game = new TennisGame(victor, sarah)
+    (1 to 3).foreach(x => victor.winBall())
+    (1 to 3).foreach(x => sarah.winBall())
+    game.score should be("deuce")
+    victor.winBall()
+    game.score should not be "deuce"
+    sarah.winBall()
+    game.score should be("deuce")
+  }
+  "Game" should "be won by the first player to have won at least four points in total and with at least two points more than the opponent" in {
+    val victor = Player("Victor")
+    val sarah = Player("Sarah")
+    val game = new TennisGame(victor, sarah)
+    (1 to 4).foreach(x => victor.winBall())
+    (1 to 3).foreach(x => sarah.winBall())
+    game.score should not be "Victor won"
+    game.score should not be "Sarah won"
+    victor.winBall()
+    game.score should be("Victor won")
+  }
+  it should "be won by player2" in {
+    val victor = Player("Victor")
+    val sarah = Player("Sarah")
+    val game = new TennisGame(victor, sarah)
+    (1 to 3).foreach(x => victor.winBall())
+    (1 to 4).foreach(x => sarah.winBall())
+    game.score should not be "Victor won"
+    game.score should not be "Sarah won"
+    sarah.winBall()
+    game.score should be("Sarah won")
+  }
 }
